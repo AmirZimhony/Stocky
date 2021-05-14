bf13145069a96c9640f23e60bf953517bcab619b
Amir <yesiamamir@gmail.com>
2021-05-14 12:03:50 +0300

    updated index.js file

--- index.js
+++ index.js
@@ -17,89 +17,17 @@ mongoose.connect('mongodb://localhost:27017/stocky', { useNewUrlParser: true, us
     })
 
 
-
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    })
-
-    //----------practice mongo----------
-    const loserSchema = new mongoose.Schema({
-        name: String,
-        reason: String,
-        age: Number
-    });
-
-const Loser = mongoose.model('Loser', loserSchema);
-const ami = new Loser({name: "ami", reason: "virgin", age: 22});
-
-Loser.insertMany([{name:'app', reason: 'blue', age: 22}, {name: 'howard', reason: 'losie', age: 29}, ami])
-app.use(express.static(path.join(__dirname, 'public')));//makes it possible to open this file from different folders
-app.use(express.urlencoded({ extended:true }));//allows us to parse  html data from requests
-app.use(express.json());//allows us to parse json data from requests
-app.use(methodOverride('_method'));//alows us to overide GET/POST requests, thus we are able to send PATCH/DELETE requests
-
-app.set('view engine', 'ejs');//determining engine for injecting variables into pages
-app.set('views', path.join(__dirname, '/views'));//makes it possible to open this file from different folders
-
-let losers = [
-    {
-        id:uuid(),
-        name: 'dan',
-        reason: 'virgin'
-    },
-    {
-        id:uuid(),
-        name: 'don',
-        reason: 'copycat'
-    },
-    {
-        id:uuid(),
-        name: 'danny',
-        reason: 'weak'
-    }
-]
-
-
-
-//defining responses to different requests
-app.get('/home',(req,res)=>{
-    res.render('home')
-})
-
-app.get('/loserform',(req,res)=>{
-    res.render('loser form')
-})
-
-app.get('/sampleform',(req,res)=>{
-    res.render('sampleLoser')
-})
-
-
-app.get('/losers/loserlist',(req,res)=>{
-    res.render('losers/losers list')
-})
-
-app.get('/losers',(req,res)=>{
-    res.render('losers/index', { losers } )
-})
-
-
-
     //----------define our database structure----------
     const StockSchema = new mongoose.Schema({
         name: String,
         DateOfInitialPurchase: String,
-        initialPurshaseSum: Number
+        initialPurshaseSum: Number,
+        price: Number
     });
 
 
 const Stock = mongoose.model('stock', StockSchema);
-const apple = new Stock({name: "applw", DateOfInitialPurchase: Date(), initialPurshaseSum: 5000});
+const apple = new Stock({name: "apple", DateOfInitialPurchase: Date(), initialPurshaseSum: 5000});
 
 Stock.insertMany([{name: "appleee", DateOfInitialPurchase: Date(), initialPurshaseSum: 5000},apple]);
 
