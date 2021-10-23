7d11ba75949232793865a5e2a70d1f70de723aa5
Amir <yesiamamir@gmail.com>
2021-10-23 23:58:01 +0300

    Added partials (including flash) and boilerplate for all app pages

--- routes/stocksViews.js
+++ routes/stocksViews.js
@@ -5,6 +5,7 @@ const ExpressError = require('../utils/ExpressError');
 const Stock = require("../models/stock");
 
 
+
 router.get('/home', (req, res) => {
     res.render('home')
 })
@@ -20,6 +21,10 @@ router.get('/stocks/new', catchAsync(async (req, res) => {
 
 router.get('/stocks/:id', catchAsync(async (req, res) => {
     const stock = await Stock.findById(req.params.id);
+    if (!stock){
+        req.flash('error', " No such a stock exists in Database");
+        return res.redirect('stocks/index');
+    }
     res.render('stocks/show', { stock });
 }))
 
@@ -32,6 +37,7 @@ router.post('/stocks', catchAsync(async (req, res) => {
     // if (!req.body.stock) throw new ExpressError('Invalid stock data', 400)
     const stock = new Stock(req.body.stock);
     await stock.save();
+    req.flash('success', 'successfully created a new stock!');
     res.redirect(`/stocks/${stock._id}`)
 }))
 
@@ -45,6 +51,7 @@ router.put('/stocks/:id', catchAsync(async (req, res) => {
 router.delete('/stocks/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Stock.findByIdAndDelete(id);
+    req.flash('success','Successfully deleted stock!');
     res.redirect(`/stocks/`)
 }))
 
